86d39ada2d56c475791f3f66c27a8f9be900ee85
CommanderMaks <86658006+commandermaks@users.noreply.github.com>
2026-03-27 10:13:28 +1100

    Add files via upload
    ITP Week 3 as 1

---
+++ arithmetic.py
@@ -0,0 +1,19 @@
+# Enter the three integers
+number1 = int(input("Enter the first integer Number: "))
+number2 = int(input("Enter the second integer Number: "))
+number3 = int(input("Enter the third integer Number: "))
+
+# Calculate the sum of the three integers entered
+sum_of_numbers = number1 + number2 + number3
+
+# Calculate the average of the three integers entered
+average_of_numbers = sum_of_numbers / 3
+    
+# Calculate the product of the three integers entered
+product_of_numbers = number1 * number2 * number3
+
+# Display input and calculated values
+print("The sum of the three integers is:", sum_of_numbers)
+print("The average of the three integers is:", average_of_numbers)
+print("The product of the three integers is:", product_of_numbers)
+10
